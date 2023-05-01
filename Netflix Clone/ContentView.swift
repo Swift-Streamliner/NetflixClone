ad69b8a9fe0a94082dfaa6cf445d0496a7b29989
tiger <thisisstreamliner@gmail.com>
2023-05-01 12:02:32 +0300

    Initial Commit

---
+++ Netflix Clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Netflix Clone
+//
+//  Created by MEI on 01.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netflix Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix Clone
+//
+//  Created by MEI on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

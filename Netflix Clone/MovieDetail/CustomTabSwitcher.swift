5ab15c3a9eb893c291ac123c033e0880f2783c74
tiger <thisisstreamliner@gmail.com>
2023-05-11 23:53:42 +0300

    Setup CustomTabSwitcher

--- Netflix Clone/MovieDetail/CustomTabSwitcher.swift
+++ Netflix Clone/MovieDetail/CustomTabSwitcher.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CustomTabSwitcher: View {
     
+    @State private var currentTab: CustomTab = .episodes
+    
     var tabs: [CustomTab]
     
     func widthForTab(_ tab: CustomTab) -> CGFloat {
@@ -20,28 +22,37 @@ struct CustomTabSwitcher: View {
         VStack {
             // Scrollable Picker
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(spacing: 20) {
                     ForEach(tabs, id: \.self) { tab in
                         VStack {
                             // Red Bar
                             Rectangle()
                                 .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(tab == currentTab ? .red : .clear)
                             // Button
                             Button(action: {
-                                
+                                currentTab = tab
                             }, label: {
                                 Text(tab.rawValue)
                                     .font(.system(size: 16, weight: .bold))
-                                    .buttonStyle(PlainButtonStyle())
+                                    .foregroundColor(tab == currentTab ? .white : .gray)
                             })
-                            
+                            .buttonStyle(PlainButtonStyle())
+                            .frame(width: widthForTab(tab), height: 30)
                         }
                     }
                 }
             }
             
             // Selected View
-            Text("SELECTED VIEW")
+            switch currentTab {
+            case .episodes:
+                Text("Episodes")
+            case .trailers:
+                Text("Trailers")
+            case .more:
+                Text("More")
+            }
         }
         .foregroundColor(.white )
     }
